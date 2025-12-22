c529eb56ea9a40bab7573ee28cab7bf29d3c5826
skppatil1997-stack <skppatil1997@gmail.com>
2025-12-22 10:16:33 +0000

    Add admin UI to select auction players fixed app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,5 @@
-import "@/styles/globals.css";
-import Navbar from "@/components/Navbar";
+import "../styles/globals.css";
+import Navbar from "../components/Navbar";
 
 export default function App({ Component, pageProps }) {
   return (
@@ -9,3 +9,4 @@ export default function App({ Component, pageProps }) {
     </>
   );
 }
+
